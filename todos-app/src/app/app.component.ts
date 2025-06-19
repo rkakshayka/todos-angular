3f67fb3295bbf24f28290fe00965fabca97fb72f
rkakshayka <60akshaykaushik06@gmail.com>
2025-06-19 11:12:46 +0530

    generated 3 components - login, welcome & error for initial setup.

--- todos-app/src/app/app.component.ts
+++ todos-app/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { ErrorComponent } from "./error/error.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LoginComponent, WelcomeComponent, ErrorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todos-app';
+  /**
+   * AppComponent serves as the root component for the Angular application.
+   * It includes the main layout and navigation for the Todos application.
+   */
+
+  // Title for the application
+  appTitle = 'Todos Application';
+  // Default message for the welcome component
+  message = 'Welcome to'
 }
