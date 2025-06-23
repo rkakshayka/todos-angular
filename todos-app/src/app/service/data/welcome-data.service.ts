c37bd750f58da9c8e691540d8700d120f124ce2b
rkakshayka <60akshaykaushik06@gmail.com>
2025-06-23 14:26:28 +0530

    handling of successful response and error response is done.

--- todos-app/src/app/service/data/welcome-data.service.ts
+++ todos-app/src/app/service/data/welcome-data.service.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+// This service is used to interact with the backend API to fetch welcome messages.
+export class HelloWorldBean {
+  constructor(public message: string) { }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,8 +14,16 @@ export class WelcomeDataService {
   constructor(private http: HttpClient) { }
 
   executeHelloWorldBeanService() {
-    return this.http.get('http://localhost:9090/hello-world-bean');
+    // This method makes an HTTP GET request to the backend API to fetch a welcome message.
+    // The URL 'http://localhost:9090/hello-world-bean' is the endpoint that returns a HelloWorldBean object.
+    // The method returns an Observable of type HelloWorldBean, which can be subscribed to in components. 
+    return this.http.get<HelloWorldBean>('http://localhost:9090/hello-world-bean');
     //console.log('Execute Hello World Bean Service');
   }
 
+  executeHelloWorldBeanServiceWithPathVariable(name: string) {
+    // use backtick `` to create a template literal for the URL
+    return this.http.get<HelloWorldBean>(`http://localhost:9090/hello-world/path-variable/${name}`);
+    //console.log('Execute Hello World Bean Service');
+  }
 }
--- todos-app/src/app/welcome/welcome.component.ts
+++ todos-app/src/app/welcome/welcome.component.ts
@@ -1,19 +1,20 @@
 import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { WelcomeDataService } from '../service/data/welcome-data.service';
+import { HelloWorldBean, WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
   standalone: true,
-  imports: [RouterLink] // Added
+  imports: [RouterLink, NgIf] // Added
 })
 
 export class WelcomeComponent implements OnInit {
 
   message = 'Some Welcome Message'
+  welcomeMesageFromService: string = ''
   name = ''
 
   //ActivatedRouter is a service provided by angular router 
@@ -29,10 +30,47 @@ export class WelcomeComponent implements OnInit {
     this.name = this.route.snapshot.params['name'];
   }
 
-  getWelcomeMessager() {
-    //console.log("Get Welcome Message Clicked");
+  getWelcomeMessage() {
     console.log(this.service.executeHelloWorldBeanService());
-    this.service.executeHelloWorldBeanService().subscribe();
+    // This method is called when the user clicks the button to get a welcome message.
+    // It calls the service method to fetch the welcome message and subscribes to the response.
+    // On success, it calls handleSuccessfulResponse to process the response.
+    // On error, it logs the error to the console.  
+
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    );
+    console.log("last line of getWelcomeMessage method");
   }
+
+
+  getWelcomeMessagerWithName() {
+    console.log(this.service.executeHelloWorldBeanServiceWithPathVariable(this.name));
+    // This method is called when the user clicks the button to get a welcome message with a name.
+    // It calls the service method with the name parameter and subscribes to the response.
+    // On success, it calls handleSuccessfulResponse to process the response.
+    // On error, it logs the error to the console.  
+    this.service.executeHelloWorldBeanServiceWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)    
+    )
+  }
+
+  handleSuccessfulResponse(response: HelloWorldBean) {
+    this.welcomeMesageFromService = response.message;
+    console.log(response);
+    console.log(response.message);
+    //this.message = response.message;
+  }
+
+  handleErrorResponse(error: any) {
+    console.log(error);
+    console.log(error.error);
+    console.log(error.error.message);
+    this.welcomeMesageFromService = error.error.message; // Assuming the error response has a message property
+    
+    // Handle the error response here, e.g., show an error message to the user
+  } 
 
 }
